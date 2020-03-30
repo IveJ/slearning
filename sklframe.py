e1b90b7d6074fe0891cbcaa4c1dd35e3b91d987f
Jevi <datacentralize@gmail.com>
2020-03-30 22:51:08 +0700

    Delete sklframe.py

--- sklframe.py
+++
@@ -1,7 +0,0 @@
-from sklearn.datasets import load_boston
-from sklearn.pipeline import Pipeline
-
-X, y = load_boston(return_X_y = True)
-pipe = Pipeline([ ("scale", StandardScaler()),
-                  ("model", LinearRegression())\n
-               ])
